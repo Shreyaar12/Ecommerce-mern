94a3f0e9b90e18efbb2fd4c56d25ebe64661cbd1
Shreya Rajpal <shreyarajpal6@gmail.com>
2023-10-30 19:59:09 +0530

    Links added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
 import React from 'react'
-import Header from './components/Header';
 import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
-
 const App = () => {
   return (
     <>
     <Header />
     <main className="py-3">
       {/* py 3 is padding on the y axis on top and bottom  */}
-      <Container>
-      <HomeScreen />
-      </Container>
-
-
-    </main>
-
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Navbar, Nav, Container, NavbarCollapse } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png'
+import {LinkContainer } from 'react-router-bootstrap';
 const Header = () => {
   return (
 <header>
@@ -9,13 +10,24 @@ const Header = () => {
 {/* lg will help hamburger menu show up , md will not show hamburger menu until you go smaller*/}
 
 <Container>
-    <Navbar.Brand href="/" >
-        <img src={logo} alt="proshop" /> Ecomm hub</Navbar.Brand>
+    <LinkContainer to='/'>
+            <Navbar.Brand>Ecomm Hub</Navbar.Brand>
+            </LinkContainer>
+
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className="ms-auto">
             {/* ms-auto aligns to the right */}
-            <Nav.Link href="/cart"><FaShoppingCart />Cart</Nav.Link>
+            <LinkContainer to='/cart'>
+            <Nav.Link >
+                <FaShoppingCart />Cart
+                </Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/login'>
+            <Nav.Link >
+                <FaUser />Sign In
+                </Nav.Link>
+                </LinkContainer>
 
         </Nav>
     </Navbar.Collapse>
