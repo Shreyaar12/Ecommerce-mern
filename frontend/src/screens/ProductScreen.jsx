715e4fc481ad5db632da88a28c47f5bc135f1117
Shreya Rajpal <shreyarajpal6@gmail.com>
2023-10-30 22:01:14 +0530

    product page more functionality added

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -28,6 +28,8 @@ const ProductScreen = () => {
 
             </ListGroup.Item>
             <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item> Description: {product.description} </ListGroup.Item>
+
         </ListGroup>
         </Col>
         <Col md={3}>
@@ -48,6 +50,14 @@ const ProductScreen = () => {
                         <strong>${product.countInStock >0 ? 'In Stock' : 'Out of Stock'}</strong>
                         </Col>
                     </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <Button 
+                    className='btn-block'
+                    type='button'
+                    disabled ={product.countInStock===0}>
+                        Add To Cart
+                    </Button>
                 </ListGroup.Item>
             </ListGroup>
         </Card>
