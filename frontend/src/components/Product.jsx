f58d8cb2ad34ea8d3abbf49560dfd7621033fd7f
Shreya Rajpal <shreyarajpal6@gmail.com>
2023-10-30 20:09:25 +0530

    Rating added

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import { Card } from "react-bootstrap";
 import { Link } from 'react-router-dom';
+import Rating from './Rating';
 import React from 'react'
 
 const Product = ({product}) => {
@@ -10,10 +11,14 @@ const Product = ({product}) => {
     </Link>
 <Card.Body>
     <Link to={`/product/${product._id}`}>
-        <Card.Title as="div">
+        <Card.Title as="div" className="product-title">
         <strong> {product.name} </strong>
         </Card.Title>
     </Link>
+    <Card.Text as='div'>
+
+        <Rating value={product.rating} text ={`${product.numReviews}`} />
+    </Card.Text>
     <Card.Text as="h3">
         ${product.price}
     </Card.Text>
