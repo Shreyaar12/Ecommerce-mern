ebe901b636400f798cb9337ab9e60437ab3b1b15
Shreya Rajpal <shreyarajpal6@gmail.com>
2023-11-01 20:44:32 +0530

    cart slice added

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,13 @@
+//no need to use api so cartSlice function
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState= localStorage.getItem("cart") ? JSON.parse (localStorage.getItem("cart")) : {cartItems:[]};
+
+const cartSlice= createSlice ({
+    name:"cart",
+    initialState,
+    reducers:{}
+});
+
+export default cartSlice.reducer;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
+import cartSliceReducer from './slices/cartSlice.js'
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer,
+    // reducer load krre idhar
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
