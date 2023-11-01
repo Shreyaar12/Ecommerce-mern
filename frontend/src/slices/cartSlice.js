9e38be3c08dda2c6f1a292da6d60d8ec439cd189
Shreya Rajpal <shreyarajpal6@gmail.com>
2023-11-02 00:37:51 +0530

    add To cart functionality added

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -17,7 +17,7 @@ const cartSlice= createSlice ({
             if (existItem){
                 state.cartItems= state.cartItems.map((x)=> x._id ===existItem._id ?item:x);
             }else{
-                state.cartItems = [...state.cartItemsa, item];
+                state.cartItems = [...state.cartItems, item];
             }
             
        
