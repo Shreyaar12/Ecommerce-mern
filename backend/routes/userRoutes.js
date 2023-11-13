9063ca53b246fe36a2df5659375f26f9bb49ccf0
Shreya Rajpal <shreyarajpal6@gmail.com>
2023-11-13 16:10:28 +0530

    auth added

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+ import {authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUserById,
+    updateUser} from '../controllers/userController.js';
+const router= express.Router();
+//everything connected to /api/users
+router.route('/').post(registerUser).get(getUsers);
+router.post('/logout',logoutUser); //kahi jana ni h bas logout hona h 
+router.post('/login',authUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 connectDB();
@@ -12,6 +13,7 @@ const app = express();
 
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 
 app.get('/', (req, res) => {
