0cbf0c5076feb413a6a4d44d9833a0ae8b832621
Shreya Rajpal <shreyarajpal6@gmail.com>
2023-11-15 01:18:08 +0530

    logout added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 // import logo from '../assets/logo.png'
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
 import {LinkContainer } from 'react-router-bootstrap';
 const Header = () => {
    const { cartItems} = useSelector((state)=> state.cart);
    const { userInfo } = useSelector((state) => state.auth);
 
-   const logoutHandler = () => {
-     console.log('logout');
-   };
+
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
+   const [ logoutApiCall] = useLogoutMutation();
+   const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login');
+    } catch (err) {
+      console.error(err);
+    }}
   return (
 <header>
 <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect> 
@@ -72,4 +84,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default Header;
