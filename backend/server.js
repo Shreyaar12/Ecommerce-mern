ed197b05961480f60f9fc8935affb5dcc7a0d702
Shreya Rajpal <shreyarajpal6@gmail.com>
2023-11-16 03:30:22 +0530

    Order routes and controller functions

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-
+import orderRoutes from './routes/orderRoutes.js';
 connectDB();
 const app = express();
 app.use(express.json());
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
-
+app.use('/api/orders', orderRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
