8278f28e6369c7d7fa022a03116ba520d9bb1913
Shreya Rajpal <shreyarajpal6@gmail.com>
2023-11-18 19:20:24 +0530

    product create new added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -20,5 +20,24 @@ if (product) {
 res.status(404);
 throw new Error('Resource not found'); } });
 // the method that mongoose uses are asynchronous islie idhar async handler
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
 
-export { getProducts, getProductById};
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+export { getProducts, getProductById, createProduct };
