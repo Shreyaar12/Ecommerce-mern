cac5dc81b1102ef693d99edc083451401268ae59
Shreya Rajpal <shreyarajpal6@gmail.com>
2023-11-19 21:04:41 +0530

    pagination homescreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import { Row, Col } from 'react-bootstrap';
-import { useGetProductsQuery } from '../slices/productSlice';
+import { useParams } from 'react-router-dom';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const {pageNumber}= useParams();
+  const { data, isLoading, error } = useGetProductsQuery({pageNumber});
 
   return (
     <>
@@ -16,7 +18,7 @@ const HomeScreen = () => {
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
           //  {/* column is responsive in react bootstrap so on small screen 1 col 12 stack on medium screen 2 cols 6 stack and so on */}
 
            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
